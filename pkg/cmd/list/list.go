c77954c83bbe7d7c05f192a5f9846232267bd6c6
lnxwizard <alperakca79@outlook.com>
2023-07-19 14:43:46 +0300

    Added new flag for list command
    Now you can list only completed to-do's with `-c, --completed` flag.

--- pkg/cmd/list/list.go
+++ pkg/cmd/list/list.go
@@ -7,7 +7,7 @@ import (
 
 func NewCmdList() *cobra.Command {
 	// suggest strings for list
-	suggestionStrings := []string{"llist", "lsit", "li", "lis", "lsi", "ls", "listt", "llist", "liist", "lisst"}
+	suggestionStrings := []string{"llist", "lsit", "li", "lis", "lsi", "ls", "listt", "llistt", "liist", "lisst"}
 
 	// define `list` command
 	cmd := &cobra.Command{
@@ -15,10 +15,30 @@ func NewCmdList() *cobra.Command {
 		SuggestFor: suggestionStrings,
 		Short:      "List all to-do's.",
 		Example:    "$ todo list",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// get value of flag: completed
+			completedFlgVal, err := cmd.Flags().GetBool("completed")
+			if err != nil {
+				return err
+			}
+
+			switch {
+			case completedFlgVal:
+				todo.Todo.ListCompleted()
+				return nil
+			}
+
 			todo.Todo.List()
+
+			return nil
 		},
 	}
+
+	// define flags
+	var (
+		completedFlg bool
+	)
+	cmd.Flags().BoolVarP(&completedFlg, "completed", "c", false, "List completed to-do's.")
 
 	return cmd
 }
--- pkg/todo/list.go
+++ pkg/todo/list.go
@@ -62,3 +62,51 @@ func (t *TodoList) List() {
 	// print table
 	table.Println()
 }
+
+// List completed to-do's
+func (t *TodoList) ListCompleted() {
+	// create new table
+	table := simpletable.New()
+
+	// set table header
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "CreatedAt"},
+			{Align: simpletable.AlignCenter, Text: "CompletedAt"},
+		},
+	}
+
+	// define table body cells
+	var cells [][]*simpletable.Cell
+
+	// add table body items
+	for index, item := range *t {
+		if item.Done {
+			index++
+
+			task := item.Task
+			createdAt := item.CreatedAt.Format(time.RFC822)
+			completedAt := item.CompletedAt.Format(time.RFC822)
+
+			cells = append(cells, *&[]*simpletable.Cell{
+				{Text: fmt.Sprintf("%d", index)},
+				{Text: task},
+				{Text: createdAt},
+				{Text: completedAt},
+			})
+		} else {
+			continue
+		}
+	}
+
+	// set table body
+	table.Body = &simpletable.Body{Cells: cells}
+
+	// set table style
+	table.SetStyle(simpletable.StyleUnicode)
+
+	// print table
+	table.Println()
+}
