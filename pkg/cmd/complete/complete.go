97d27e59ab54db5b13a7f2015b4327b6f9836103
lnxwizard <alperakca79@outlook.com>
2023-07-20 13:48:47 +0300

    Added new flag for `complete` command
    You can mark all to-do's as completed with `todo complete --all` command

--- pkg/cmd/complete/complete.go
+++ pkg/cmd/complete/complete.go
@@ -1,6 +1,7 @@
 package complete
 
 import (
+	"errors"
 	"github.com/lnxwizard/todo/internal/input"
 	"github.com/lnxwizard/todo/pkg/todo"
 	"github.com/spf13/cobra"
@@ -17,6 +18,26 @@ func NewCmdComplete() *cobra.Command {
 		Short:      "Mark a to-do as completed by index.",
 		Example:    "$ todo complete 2",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			allFlgVal, err := cmd.Flags().GetBool("all")
+			if err != nil {
+				return err
+			}
+
+			switch {
+			case allFlgVal:
+				todo.Todo.CompleteAll()
+
+				if err := todo.Todo.Store(todo.TodoFile); err != nil {
+					return err
+				}
+
+				return nil
+			}
+
+			if len(args) < 3 {
+				return errors.New("index cannot be empty")
+			}
+
 			index, err := input.GetIntegerInput()
 			if err != nil {
 				return err
@@ -33,6 +54,12 @@ func NewCmdComplete() *cobra.Command {
 			return nil
 		},
 	}
+
+	// define flags
+	var (
+		allFlg bool
+	)
+	cmd.Flags().BoolVarP(&allFlg, "all", "a", false, "Mark all to-do's as completed.")
 
 	return cmd
 }
--- pkg/todo/complete.go
+++ pkg/todo/complete.go
@@ -18,3 +18,17 @@ func (t *TodoList) Complete(index int) error {
 
 	return nil
 }
+
+// Mark all to-do's as completed
+func (t *TodoList) CompleteAll() {
+	tdls := *t
+
+	for i := 0; i < len(*t); i++ {
+		if tdls[i].Done {
+			continue
+		}
+
+		tdls[i].CompletedAt = time.Now()
+		tdls[i].Done = true
+	}
+}
